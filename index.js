4a189f8df421d72567b69c3188334780cbe19237
Sávio Almeida <saviornelas+github@gmail.com>
2024-07-30 15:30:56 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,132 @@
+document.getElementsByTagName("input")[0].focus();
+
+document.getElementsByTagName("input")[0].addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementsByClassName("button")[0].click();
+    }
+});
+
+
+document.getElementsByClassName("button")[0].addEventListener("click", function () {
+    let raiz = document.getElementsByTagName("input")[0].value;
+
+    raiz = raiz.split(".").join("").split("/").join("").split("-").join("");
+
+    let dv = "";
+
+    switch (document.getElementsByTagName("select")[0].value) {
+        case "cnpj":
+            if (isNaN(raiz)) {
+                window.alert("Informe o CNPJ apenas com dígitos.");
+            }
+
+            if (raiz.length > 8) {
+                window.alert("Informe apenas a raiz do CNPJ.");
+            }
+
+            raiz = raiz.padStart(8, "0") + "0001";
+
+            dv = dvCnpj(raiz);
+
+            break;
+
+        case "estab":
+            if (isNaN(raiz)) {
+                window.alert("Informe o CNPJ apenas com dígitos.");
+            }
+
+            if (raiz.length > 12) {
+                window.alert("Informe apenas a raiz do CNPJ.");
+            }
+
+            raiz = raiz.padStart(12, "0");
+
+            dv = dvCnpj(raiz);
+
+            break;
+
+        case "cpf":
+            if (isNaN(raiz)) {
+                window.alert("Informe o CPF apenas com dígitos.");
+            }
+
+            if (raiz.length > 9) {
+                window.alert("Informe apenas a raiz do CPF.");
+            }
+
+            raiz = raiz.padStart(9, "0");
+
+            dv = dvCpf(raiz);
+
+            break;
+    }
+
+    document.getElementById("resultado").innerText = raiz + dv;
+});
+
+function dvCnpj(raiz) {
+    var pesos = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+    var soma1 = 0;
+    var soma2 = 0;
+    var digito = 0;
+
+    for (var i = 0; i < 12; i++) {
+        digito = parseInt(raiz[i]);
+        soma1 += digito * pesos[i + 1];
+        soma2 += digito * pesos[i]
+    }
+
+    resto1 = soma1 % 11;
+
+    if (resto1 < 2) {
+        dv1 = 0;
+    } else {
+        dv1 = 11 - resto1;
+    }
+
+    soma2 += 2 * dv1;
+    resto2 = soma2 % 11;
+
+    if (resto2 < 2) {
+        dv2 = 0;
+    } else {
+        dv2 = 11 - resto2;
+    }
+
+    return `${dv1}${dv2}`;
+}
+
+function dvCpf(raiz) {
+    var pesos = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+    var soma1 = 0;
+    var soma2 = 0;
+    var digito = 0;
+
+    for (var i = 0; i < 9; i++) {
+        digito = parseInt(raiz[i]);
+        soma1 += digito * pesos[i + 1];
+        soma2 += digito * pesos[i]
+    }
+
+    resto1 = soma1 % 11;
+
+    if (resto1 < 2) {
+        dv1 = 0;
+    } else {
+        dv1 = 11 - resto1;
+    }
+
+    soma2 += 2 * dv1;
+    resto2 = soma2 % 11;
+
+    if (resto2 < 2) {
+        dv2 = 0;
+    } else {
+        dv2 = 11 - resto2;
+    }
+
+    return `${dv1}${dv2}`;
+}
